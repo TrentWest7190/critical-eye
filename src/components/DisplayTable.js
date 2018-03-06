c1515beebf9f7c91197f1019c441739cbd0ccd2c
Trent West <Trent.West@motivedrilling.com>
2018-03-06 16:17:26 -0600

    sharpness bars

--- src/components/DisplayTable.js
+++ src/components/DisplayTable.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactTable from 'react-table'
+import styled from 'styled-components'
 import 'react-table/react-table.css'
 
 export default function DisplayTable(props) {
@@ -23,7 +24,7 @@ export default function DisplayTable(props) {
         {
             Header: 'Sharpness',
             accessor: 'sharpness',
-            Cell: props => console.log(props)
+            Cell: props => <SharpnessBar data={props.value}/>
         }
     ]
     return(
@@ -31,5 +32,33 @@ export default function DisplayTable(props) {
             columns={columns}
             data={props.data}
         />
+    )
+}
+
+const FlexBar = styled.div`
+    display: flex;
+    height: 14px;
+    border: 1px solid black;
+    width: 160px;
+    background-color: gray;
+`
+
+function SharpnessBar(props) {
+    const sharp = {
+        red: props.data.red,
+        orange: props.data.orange,
+        yellow: props.data.yellow,
+        green: props.data.green,
+        blue: props.data.blue,
+        white: props.data.white,
+    }
+    return (
+        <FlexBar>
+            {
+                Object.keys(sharp).map((x) => {
+                    return (<div key={x} style={{ width: sharp[x] * .4, backgroundColor: x }}></div>)
+                })
+            }
+        </FlexBar>
     )
 }
